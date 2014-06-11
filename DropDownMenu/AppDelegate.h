150de7bc760a5f46dea7adeb9b1690af5bd1eae8
Kevin Chen <xmkevinchen@gmail.com>
2014-06-11 10:07:45 -0700

    Initialize Project

---
+++ DropDownMenu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DropDownMenu
+//
+//  Created by Kevin Chen on 6/11/14.
+//  Copyright (c) 2014 KnightLord Universe Technolegies Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DropDownMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DropDownMenu
+//
+//  Created by Kevin Chen on 6/11/14.
+//  Copyright (c) 2014 KnightLord Universe Technolegies Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
